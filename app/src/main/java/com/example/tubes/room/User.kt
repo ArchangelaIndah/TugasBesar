9a56a10a7fe6fdf49088dc68323a3447100b8be9
tangkasR <tangkasrisdianto2@gmail.com>
2022-09-22 15:38:43 +0700

    buat database

---
+++ app/src/main/java/com/example/tubes/room/User.kt
@@ -0,0 +1,15 @@
+package com.example.tubes.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val nama: String,
+    val email: String,
+    val tglLahir: String,
+    val alamat: String,
+    val noTelp: String
+)
---
+++ app/src/main/java/com/example/tubes/room/UserDao.kt
@@ -0,0 +1,16 @@
+package com.example.tubes.room
+import androidx.room.*
+
+@Dao
+interface UserDao {
+    @Insert
+    suspend fun addUser(user: User)
+    @Update
+    suspend fun updateUser(user: User)
+    @Delete
+    suspend fun deleteUser(user: User)
+    @Query("SELECT * FROM user")
+    suspend fun getUsers() : List<User>
+    @Query("SELECT * FROM user WHERE id =:user_id")
+    suspend fun getUser(user_id: Int) : List<User>
+}
